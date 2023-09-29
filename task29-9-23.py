f77937b65539236cb7bb18c47d1e8fb973843b59
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-09-29 16:27:49 +0530

    Add files via upload

---
+++ task29-9-23.py
@@ -0,0 +1,109 @@
+#arthematic
+#addition
+a = 10
+b = 15
+print(a+b)
+
+#substraction
+a = 10
+b = 15
+print(a-b)
+
+#multification
+a = 10
+b = 15
+print(a*b)
+
+#division
+a = 15
+b = 55
+print(a/b)
+
+#module
+a = 10
+b = 15
+print(a%b)
+
+#floordivison
+a = 10
+b = 15
+print(a//b)
+
+#assignment oparator
+#equal
+a = 10
+a += 1
+print(a)
+
+#addition equal
+a = 10
+a += 5
+print(a)
+
+#substraction equal
+a = 10
+a -= 5
+print(a)
+
+#multiflication equal
+a = 10
+a *= 2
+print(a)
+
+#division equal
+a = 10
+a /= 5
+print(a)
+
+#module equal
+a = 9
+a %= 2
+print(a)
+
+#floor division
+a = 9
+a //= 2
+print(a)
+
+#and oparator
+a = 5
+b = 10
+c = 7
+print(a<b and a<c)
+
+#or oparator
+a = 5
+b = 10
+c = 7
+print(a<b or a>c)
+
+#not oparator
+a = 5
+b = 10
+c = 7
+print(not(a<b and a<c))
+
+#comparision oparator
+a=10
+b=10
+print(a <= b)
+
+#bitwise oparators
+#and oparator
+a=6
+b=2
+print(a & b)
+print(6 & 2)
+
+#or oparator
+a=6
+b=2
+print(a | b)
+print(18 | 2)
+
+#Xor oparator
+a=6
+b=2
+print(a ^ b)
+print(6 ^ 2)
+
