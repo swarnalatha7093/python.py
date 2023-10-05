ad3534d8d66697f8a8459dd65ec72c74f05ec097
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-10-05 21:58:02 +0530

    task5-10-23.py

--- task5-10-23.py
+++ task5-10-23.py
@@ -14,21 +14,20 @@ else:
 if accnumber == 123456789 and pin == 12345:
     print("Account number is valid")
     print("select the number 1 is withdraw option.")
-    print("select the number 2 is balence enquiry option.")
+    print("select the number 2 is balance enquiry option.")
     option = int(input("click the option 1 or 2: "))
     if option == 1:
-        if(option==1):
-            debit=int(input("how much amount withdraw: "))
-            if(debit>150000):
-                print("Insufficient balance.")
-            else:
-                option=(150000-debit)
-                print("Amount succesfully debited")
-                print("your bank balance:",option)
-        elif(option==2):
-            print("total bank balnce = 150000")
+        debit=int(input("how much amount withdraw: "))
+        if(debit>150000):
+            print("Insufficient balance.")
         else:
-            print("click the valid option")
+            option=(150000-debit)
+            print("Amount succesfully debited")
+            print("your bank balance:",option)
+    elif(option==2):
+        print("total bank balnce = 150000")
+    else:
+        print("click the valid option")
 else:
     print("invalid accnumber and pin:")
     print("please enter the valid details.")
