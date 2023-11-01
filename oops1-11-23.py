1950274855d34f0e01523ccddc308a496ab98a4c
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-11-01 13:06:29 +0530

    Add files via upload

---
+++ oops1-11-23.py
@@ -0,0 +1,50 @@
+"""#global scope:
+class Employee:
+    firstName : str = "sony"
+    lastName : str = "swarna"
+emp = Employee()
+print(emp.firstName)
+print(emp.lastName)
+
+#partially private:
+class Employee:
+    _firstName : str = "sony"
+    _lastName : str = "swarna"
+emp = Employee()
+print(emp._firstName)
+
+#strictly private
+class Employee:
+    __firstName : str = "sony"
+    __lastName : str ="swarna"
+emp = Employee()
+print(emp.__firstName)
+
+#globalvariable
+def fullName():
+    global lName
+    fName = "prasa"
+    lName = "prema"
+fullName()
+print(lName)
+print(fName)  #error
+
+#function scope
+def fullName():
+    global fName
+    lName = "sony"
+    fName = "ammu"
+fullName()
+print(fName)
+print(lName) #error
+"""
+#block scope
+class Employee:
+    __firstName : str ="sony"
+    __lastName : str = "swarna"
+    def fullName(self):
+        return self.__firstName + " " + self.__lastName
+emp = Employee()
+emp.__firstName = "ABC"
+print(emp.fullName())
+print(emp.__firstName)
